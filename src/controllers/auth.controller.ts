87056f6636f654c4b007f23c2c8134bc649a84e5
pallascodes <pallascodes@gmail.com>
2025-05-16 12:24:03 -0600

    feat: implement authentication endpoint with login functionality

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express'
+import { getConnection } from '../config/db'
+
+export const authRouter = Router()
+
+authRouter.post('/login', async (req, res) => {
+  const { username, password } = req.body
+  console.log('🚀 ~ authRouter.post ~ req.body:', req.body)
+
+  const pool = await getConnection()
+  const result = await pool
+    .request()
+    .input('username', username)
+    .input('password', password)
+    .query(
+      'SELECT * FROM dbo.usuarioKiosco WHERE username = @username AND password = @password'
+    )
+
+  if (result.recordset.length === 0) {
+    res.status(401).json({ message: 'Credenciales invalidas' })
+    return
+  }
+
+  const { password: _, ...user } = result.recordset[0]
+
+  res.status(200).json({ message: 'Login exitoso', user })
+})
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 
 import { estadoCtaRouter } from '../controllers/estado-cuenta.controller'
 import { clienteRouter } from '../controllers/cliente.controller'
+import { authRouter } from '../controllers/auth.controller'
 
 const router = Router()
 
 router.use('/estado-cuenta', estadoCtaRouter)
 router.use('/cliente', clienteRouter)
+router.use('/auth', authRouter)
 
 export default router
