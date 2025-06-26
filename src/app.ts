5fe89f647129057b26b72ce1cc55d5eceb38eae6
pallascodes <pallascodes@gmail.com>
2025-06-26 13:01:41 -0600

    feat: implement CORS middleware with dynamic origin whitelist

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,29 @@
-import express from 'express'
+import cors, { CorsOptions } from 'cors'
 import dotenv from 'dotenv'
-import routes from './routes'
+import express from 'express'
+
 import { errorMiddleware } from './middlewares/error.middleware'
-import cors from 'cors'
+import routes from './routes'
 
 dotenv.config()
 
 export const app = express()
 
+const whitelist: string[] = process.env.CORS_WHITELIST?.split(',') ?? []
+
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('No permitido por CORS'))
+    }
+  },
+  credentials: true,
+}
+
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.static('public'))
 app.use('/api', routes)
 app.use(errorMiddleware)
